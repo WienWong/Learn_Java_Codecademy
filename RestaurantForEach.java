576a09f2f15d7cecbca3a4255212fa825469345e
Weihua Wang <wangzichengfu@gmail.com>
2017-12-08 07:40:28 +0100

    Create RestaurantForEach.java
    1V

---
+++ RestaurantForEach.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class RestaurantForEach {
+	public static void main(String[] args) {
+
+		HashMap<String, Integer> restaurantMenu = new HashMap<String, Integer>();
+
+		restaurantMenu.put("Turkey Burger", 13);
+		restaurantMenu.put("Naan Pizza", 11);
+		restaurantMenu.put("Cranberry Kale Salad", 10);
+
+		System.out.println(restaurantMenu.size());
+
+		for (String item : restaurantMenu.keySet()) {
+
+			System.out.println("A " + item + " costs " + restaurantMenu.get(item) + " dollars.");
+
+		}
+	}
+}
+
+/*
+Output
+3
+A Turkey Burger costs 13 dollars.
+A Cranberry Kale Salad costs 10 dollars.
+A Naan Pizza costs 11 dollars.
+*/
