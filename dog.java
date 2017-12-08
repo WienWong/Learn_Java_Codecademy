189dd87b51f1ba252bba5554148f570c693083d2
Weihua Wang <wangzichengfu@gmail.com>
2017-12-08 06:55:03 +0100

    Update dog.java
    1V

--- dog.java
+++ dog.java
@@ -1,23 +1,24 @@
+
 class Animal {
-  public void checkStatus() {
-    System.out.println("Your pet is healthy and happy!");
+	public void checkStatus() {
+		System.out.println("Your pet is healthy and happy!");
 	}
 }
 
 class Dog extends Animal {
-  int age;
+	int age;
 	public Dog(int dogsAge) {
-    age=dogsAge;
-  }
-  public void bark() {
-  	System.out.println("Woof!");
-  }
-  public void run(int feet){
-  	System.out.println("Your dog ran " + feet + " feet!");
-  }
-  public int getAge(){
-  	return age;
-  }
+		age=dogsAge;
+	}
+	public void bark() {
+		System.out.println("Woof!");
+	}
+	public void run(int feet){
+		System.out.println("Your dog ran " + feet + " feet!");
+	}
+	public int getAge(){
+		return age;
+	}
 
 	public static void main(String[] args) {
 		Dog spike=new Dog(2);
@@ -28,6 +29,7 @@ class Dog extends Animal {
 		spike.checkStatus(); // method inherit from Animal
 	}
 }
+
 
 /*
 Output:
