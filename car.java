83d468641f5a7360967d362ed6b4f220ffee6472
Weihua Wang <wangzichengfu@gmail.com>
2017-12-08 07:00:59 +0100

    Create car.java
    1V

---
+++ car.java
@@ -0,0 +1,49 @@
+class Vehicle {
+
+    public void checkBatteryStatus() {
+
+        System.out.println("The battery is fully charged and ready to go!");
+
+    }
+}
+
+class Car extends Vehicle {
+
+    int modelYear;
+
+    public Car(int year) {
+
+        modelYear = year;
+
+    }
+
+    public void startEngine() {
+
+        System.out.println("Vroom!");
+
+    }
+
+    public void drive(int distanceInMiles) {
+
+        System.out.println("Your car drove " + distanceInMiles + " miles!");
+
+    }
+
+    public int numberOfTires() {
+
+        return 4;
+
+    }
+
+    public static void main(String[] args){
+
+        Car myFastCar = new Car(2007)
+        myFastCar.startEngine();
+        myFastCar.drive(1628);
+        myFastCar.checkBatteryStatus();
+
+        int tires = myFastCar.numberOfTires();
+        System.out.println(tires);
+
+    }
+}
